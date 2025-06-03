d654e2db40b643177ba436448557ebf2c356925b
Yeskkimseul <yeslkim2@gmail.com>
2025-06-03 22:48:49 +0900

    update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,41 +1,14 @@
-import { useEffect, useState } from "react";
-import GoodsCard from "../components/GoodsCard";
+import React from 'react';
 import Layout from "../components/Layout";
-import { useGoods } from "../context/GoodsContext";
-import styles from './CardListLayout.module.css'
+import styles from '../styles/Login.module.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    //굿즈 전체 리스트와 바꾸는 함수 (전역에서 관리 중)
-    const { goodsList, setGoodsList } = useGoods();
-    //내가 찜한 굿즈 id 목록 (localstorage 기반)
-    const [likedIds, setLikedIds] = useState<string[]>([]);
-    //찜한 것만 보기 버튼 눌렀을때
-    const [showOnlyLiked, setshowOnlyLiked] = useState(false)
-    useEffect(() => {
-        const stored = localStorage.getItem('goodsList'); //저장된 굿즈
-        const liked = localStorage.getItem('likes');
-
-        //로컬 스토리지에 있으면 그걸 쓰고
-        if (stored) setGoodsList(JSON.parse(stored));
-        else {
-            fetch('data/goods.json') //불러옴
-                .then((res) => res.json()) //json형식으로 바꾸기(자바스크립트 객체로 변환)
-                .then((data) => {
-                    setGoodsList(data) //가져온 데이터를 화면에 보여줄 상태에 저장
-                    localStorage.setItem('goodsList', JSON.stringify(data));
-                    //localstorage에도 똑같이 저장 = 새로고침해도 유지되게
-                })
-        }
-        if(liked) setLikedIds(JSON.parse(liked));
-    }, [setGoodsList])
-    //보여줄 리스트 결정 : 찜필터 on이면 찜한 것만 보여주기
-    const displayedList = showOnlyLiked
-        ? goodsList.filter((item) => likedIds.includes(item.id)) /* 찜한 굿즈만 걸리게끔 필터 */
-        : goodsList;
+  
 
     return (
         <Layout>
-          <div>
+          <div className='sokcho'>
             로그인
           </div>
         </Layout>
