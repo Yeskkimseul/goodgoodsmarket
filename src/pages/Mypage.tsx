09d1c9d820fca9e12f6712501d5173fa8fc4c288
ddiong2525 <choijw2005@naver.com>
2025-06-04 17:34:22 +0900

    header7

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,10 +1,12 @@
 import Layout from "../components/Layout";
+import Header from "../components/header/Header";
 
-const Mypage = () =>{
-    return(
+const Mypage = () => {
+    return (
         <Layout>
+            <Header type="type7"></Header>
             <div>
-               마이페이지입니다
+                마이페이지입니다
             </div>
         </Layout>
     )
