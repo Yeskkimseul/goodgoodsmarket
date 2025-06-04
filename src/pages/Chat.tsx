4a465b34f6bf8fd64e6eccafb58adae6040960eb
ddiong2525 <choijw2005@naver.com>
2025-06-04 17:32:20 +0900

    header6

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,8 +1,10 @@
 import Layout from "../components/Layout";
+import Header from "../components/header/Header";
 
-const Chat = () =>{
-    return(
+const Chat = () => {
+    return (
         <Layout>
+            <Header type="type6"></Header>
             <div>
                 챗페이지입니다
             </div>
