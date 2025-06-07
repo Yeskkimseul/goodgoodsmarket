d2a88aa073ece4f48aef0d3eda3173a0cdd0f99b
ddiong2525 <choijw2005@naver.com>
2025-06-07 17:52:44 +0900

    commudetailheader

--- src/pages/CommuDetail.tsx
+++ src/pages/CommuDetail.tsx
@@ -1,8 +1,10 @@
 import Layout from "../components/Layout";
+import Header from "../components/header/Header";
 
-const CommuDetail = () =>{
-    return(
+const CommuDetail = () => {
+    return (
         <Layout>
+            <Header type="type1" title="커뮤니티" />
             <div>
                 커뮤니티 디테일
             </div>
