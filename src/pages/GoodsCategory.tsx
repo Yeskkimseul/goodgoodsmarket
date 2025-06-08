446a7ef38e995bec2a490fc19c28a8ffbc420dd8
Yeskkimseul <yeslkim2@gmail.com>
2025-06-08 17:07:53 +0900

    update

--- src/pages/GoodsCategory.tsx
+++ src/pages/GoodsCategory.tsx
@@ -3,15 +3,21 @@ import DefaultTab from "../components/exchangebuy/DefalutTab";
 import Header from "../components/header/Header";
 import styles from '../components/exchangebuy/Tab.module.css';
 
-const GoodsCategory = () =>{
-    return(
-        <Layout>
-            <Header type="type1"></Header>
+const GoodsCategory = () => {
+  return (
+    <Layout>
+      <Header type="type1"></Header>
       <div className={styles.pageContent}>
-        <DefaultTab />
+        <DefaultTab leftTab="교환" rightTab="구매">
+          {(activeTab) =>
+            activeTab === 'left'
+              ? <div>교환 탭에 원하는 컴포넌트</div>
+              : <div>구매 탭에 원하는 컴포넌트</div>
+          }
+        </DefaultTab>
       </div>
-        </Layout>
-    )
+    </Layout>
+  )
 
 }
 
