f8a537736709bf647ba7a6bc670a2d1539b1ce7b
ddiong2525 <choijw2005@naver.com>
2025-06-10 13:04:36 +0900

    왜오류나

--- src/components/TrustModal.tsx
+++ src/components/TrustModal.tsx
@@ -15,17 +15,17 @@ const TrustModal: React.FC<TrustModalProps> = ({ onClose }) => {
                 </div>{/* //.title */}
                 <div className={Styles.list}>
                     <img src="images/mypage/trust_gg.svg" alt="굿굿" />
-                    <div><h5>굿굿 - </h5> 신뢰도 70% 이상 : 믿고 거래할 수 있어요!</div>
+                    <div><h5>굿굿 - </h5>&nbsp;신뢰도 70% 이상 : 믿고 거래할 수 있어요!</div>
                 </div>{/* //.list */}
 
                 <div className={Styles.list}>
                     <img src="images/mypage/trust_g.svg" alt="굿" />
-                    <div><h5>굿 - </h5> 신뢰도 50% 이상 : 괜찮은 사용자예요.</div>
+                    <div><h5>굿 - </h5>&nbsp;신뢰도 50% 이상 : 괜찮은 사용자예요.</div>
                 </div>{/* //.list */}
 
                 <div className={Styles.list}>
                     <img src="images/mypage/trust_ss.svg" alt="쏘쏘" />
-                    <div><h5>쏘쏘 - </h5> 신뢰도 50% 미만 : 신중한 거래가 필요해요.</div>
+                    <div><h5>쏘쏘 - </h5>&nbsp;신뢰도 50% 미만 : 신중한 거래가 필요해요.</div>
                 </div>{/* //.list */}
 
             </div>
