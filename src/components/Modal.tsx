7a9d4f4d38c69ecf7f7960b150df2b50c67d29bd
ddiong2525 <choijw2005@naver.com>
2025-06-10 12:26:25 +0900

    mypage

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -21,13 +21,13 @@ const Modal: React.FC<ModalProps> = ({
     if (!isOpen) return null;
 
     return (
-        <div className={styles.overlay}>
-            <div className={styles.modal}>
+        <div className={styles.overlay} onClick={onCancel}>
+            <div className={styles.modal} onClick={e => e.stopPropagation()}>
                 <div className={styles.txt}>
                     <div className="subtit2">{title}</div>
                     <p>{description}</p>
                 </div>
-                <div className={styles.buttons}>
+                <div className={styles.buttons} >
                     <button onClick={onCancel}>취소하기</button>
                     <button className={styles.confirm} onClick={onConfirm}>{confirmText}</button>
                 </div>
