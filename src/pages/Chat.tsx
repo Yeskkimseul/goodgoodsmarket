53e9a072cdb1f41028b11cb3a1ee6b4d1dd5762f
hyerinn <hy3riim@gmail.com>
2025-06-10 14:31:56 +0900

    zindex

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,13 +1,12 @@
 import Layout from "../components/Layout";
 import Header from "../components/header/Header";
+import ChatList from "../components/ChatList";
 
 const Chat = () => {
     return (
         <Layout>
             <Header type="type6"></Header>
-            <div>
-                챗페이지입니다
-            </div>
+            <ChatList />
         </Layout>
     )
 
