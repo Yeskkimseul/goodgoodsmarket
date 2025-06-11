96c50f0030c687a2091fabfd3cee75ff5f5f71c1
Yeskkimseul <yeslkim2@gamil.com>
2025-06-11 17:02:19 +0900

    update

---
+++ src/types/reivew.d.ts
@@ -0,0 +1,21 @@
+export interface Review {
+    id: string;
+    sellerName: string; // 리뷰 대상 판매자
+    reviewer: {
+        name: string;
+        img: string;
+    };
+    reviewimg?: {
+        img: string;
+    }
+    date: string;
+    content?: string; // (선택) 상세 리뷰 내용
+    keywords: string[]; // 리뷰 키워드
+    goods: {
+        id: string;      // goods.json의 id
+        title: string;
+        imageUrl: string;
+    };
+    recommend: number;   // 추천수
+    
+}
