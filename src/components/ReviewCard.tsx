b18d0f33346f6df22d41076cbe87bb7dd098aa22
Yeskkimseul <yeslkim2@gamil.com>
2025-06-11 17:55:32 +0900

    update

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,11 +1,33 @@
 import style from "./ReviewCard.module.css";
+import { Review } from "../types/reivew";
+import { Link } from "react-router-dom";
 
-const ReviewCard = () => {
+interface ReviewCardProps {
+    review: Review;
+}
+
+const ReviewCard = ({ review }: ReviewCardProps) => {
     return (
-        <div>
-
+        <div className={style.card}>
+            <div className={style.header}>
+                <img src={review.reviewer.img} alt={review.reviewer.name} className={style.avatar} />
+                <span className={style.name}>{review.reviewer.name}</span>
+                <span className={style.date}>{review.date}</span>
+            </div>
+            <div className={style.keywords}>
+                {review.keywords.map((kw, idx) => (
+                    <span key={idx} className={style.keyword}>{kw}</span>
+                ))}
+            </div>
+            <div className={style.goods}>
+                <Link to={`/goods/${review.goods.id}`} className={style.goodsLink}>
+                    <img src={review.goods.imageUrl} alt={review.goods.title} className={style.goodsImg} />
+                    <span className={style.goodsTitle}>{review.goods.title}</span>
+                </Link>
+                <div className={style.recommend}>추천수: {review.recommend}</div>
+            </div>
         </div>
-    )
+    );
 };
 
 export default ReviewCard;
