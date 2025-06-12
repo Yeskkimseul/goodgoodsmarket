479ec90f036aea28dd2bf557133d195ea0bc8f04
ddiong2525 <choijw2005@naver.com>
2025-06-12 16:48:02 +0900

    about1

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,29 +9,53 @@ const About = () => {
         <Layout>
             <Header type="type1" title="굿굿마켓 소개" />
             <div className={styles.body}>
-            <Marquee>
-                <ul>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                                        <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                    <li>되나보자...</li>
-                </ul>
-            </Marquee>
 
+
+            </div>
+            <div className={styles.bgbg}>
+                <img src="/images/about/muzi.png" alt="배경" />
+                <div className={styles.marquee}>
+                    <Marquee>
+                        <div>
+                            <img src="/images/about/marquee1.svg" alt="굿굿마켓" className={styles.mi} />
+                            <img src="/images/about/marquee2.svg" alt="슬로건" className={styles.mi} />
+                            <img src="/images/about/marquee1.svg" alt="굿굿마켓" className={styles.mi} />
+                            <img src="/images/about/marquee2.svg" alt="슬로건" className={styles.mi} />
+                            <img src="/images/about/marquee1.svg" alt="굿굿마켓" className={styles.mi} />
+                            <img src="/images/about/marquee2.svg" alt="슬로건" className={styles.mi} />
+                        </div>
+                    </Marquee>
+                </div>
+                <div className={styles.imgscroll}>
+                    <div className={styles.scroll}>
+                        <Marquee direction="right" speed={30}>
+                            <div>
+                                <img src="/images/about/scrolltop1.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrolltop2.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrolltop3.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrolltop4.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrolltop5.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrolltop6.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrolltop7.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrolltop5.png" alt="굿굿마켓" />
+                            </div>
+                        </Marquee>
+                    </div>
+                    <div className={styles.scroll}>
+                        <Marquee speed={40}>
+                            <div>
+                                <img src="/images/about/scrollbottom1.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrollbottom2.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrollbottom3.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrollbottom4.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrollbottom5.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrollbottom6.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrollbottom7.png" alt="굿굿마켓" />
+                                <img src="/images/about/scrollbottom5.png" alt="굿굿마켓" />
+                            </div>
+                        </Marquee>
+                    </div>
+                </div>
             </div>
         </Layout>
     )
