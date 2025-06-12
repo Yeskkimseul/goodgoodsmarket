c743373ea2d85dfe9f3cdbba3b81cf10acd724b4
ddiong2525 <choijw2005@naver.com>
2025-06-12 14:45:39 +0900

    marquee

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,12 +2,35 @@ import React from "react";
 import Layout from "../components/Layout";
 import Header from "../components/header/Header";
 import styles from "./About.module.css"
+import Marquee from "react-fast-marquee";
 
 const About = () => {
     return (
         <Layout>
-            <Header type="type1" title="굿굿마켓 소개"/>
+            <Header type="type1" title="굿굿마켓 소개" />
             <div className={styles.body}>
+            <Marquee>
+                <ul>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                                        <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                    <li>되나보자...</li>
+                </ul>
+            </Marquee>
 
             </div>
         </Layout>
