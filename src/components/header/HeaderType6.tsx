968a43d58d7f1236de99e3f6e46541f4bedd3661
ddiong2525 <choijw2005@naver.com>
2025-06-16 16:49:46 +0900

    headerchat

--- src/components/header/HeaderType6.tsx
+++ src/components/header/HeaderType6.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Header.module.css';
-
+import { useNavigate, Link } from 'react-router-dom';
 
 const HeaderType6: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate("/home");
+    };
+
     return (
         <header className={styles.header}>
-            <h1>
-                채팅
-            </h1>
+            <div className={styles.logo} onClick={handleBack} style={{ cursor: 'pointer', justifyContent: 'center' }}>
+                <img src="/images/header/logo.svg" alt="굿굿마켓" />
+            </div>
         </header>
     );
 };
