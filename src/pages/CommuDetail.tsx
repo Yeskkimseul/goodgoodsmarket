9d1012c5690dbffa1ac61d43f6bf8e8db14923aa
Yeskkimseul <yeslkim2@gamil.com>
2025-06-16 12:59:55 +0900

    update

--- src/pages/CommuDetail.tsx
+++ src/pages/CommuDetail.tsx
@@ -317,7 +317,8 @@ const CommuDetail = () => {
                                                         ? "/images/icon/heart_small_mint_fill.svg"
                                                         : "/images/icon/heart_small_grey_empty.svg"
                                                 }
-                                                alt="좋아요"
+                                                alt="좋아요" 
+                                                className={style.commentlikesicon}
                                             />
                                             <p className={style.commentlike}>{commentLikes[c.id] ?? c.commentlikes} 좋아요</p>
                                         </div>
