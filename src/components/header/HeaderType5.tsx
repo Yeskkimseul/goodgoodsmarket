8db641c551b18e8424749ff772f1cc6937ddcbb5
hyerinn <hy3riim@gmail.com>
2025-06-16 12:45:44 +0900

    header

--- src/components/header/HeaderType5.tsx
+++ src/components/header/HeaderType5.tsx
@@ -1,28 +1,33 @@
 import React, { useState } from 'react';
 import styles from './Header.module.css';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link, useParams } from 'react-router-dom';
+import { Chatting } from '../../types/chatting';
 
 interface HeaderProps {
-  type?: string;
+  chat: Chatting | undefined;
   onMoreClick?: () => void;  // 더보기 클릭 이벤트 콜백 추가
 }
 
-const HeaderType5: React.FC<HeaderProps> = ({ type, onMoreClick }) => {
+const HeaderType5: React.FC<HeaderProps> = ({ chat, onMoreClick }) => {
   const navigate = useNavigate();
 
   const handleBack = () => {
     navigate(-1);
   };
 
+
   return (
     <header className={styles.header}>
       <ul className={styles.type5}>
-        <li onClick={handleBack} style={{ cursor: 'pointer', justifyContent: 'center' }}className={styles.back}>
+        <li onClick={handleBack} style={{ cursor: 'pointer', justifyContent: 'center' }} className={styles.back}>
           <img src="/images/header/header_back.svg" alt="뒤로가기" />
         </li>
         <li className={styles.avatar}>
-          <img src="/images/header/header_avatars.svg" alt="프로필사진" />
-          <h3>키링은 못참지</h3>
+          <img 
+          className={styles.userProfile}
+          src={chat?.userProfile}
+          alt="프로필사진" />
+          <h3>{chat?.username}</h3>
         </li>
       </ul>
       {/* onMoreClick props를 이용해서 더보기 클릭 이벤트 전달 */}
