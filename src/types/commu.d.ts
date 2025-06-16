c63ce46b3eb5f09922da9b856b90b6f6ecb937ba
Yeskkimseul <yeslkim2@gamil.com>
2025-06-16 14:40:16 +0900

    update

--- src/types/commu.d.ts
+++ src/types/commu.d.ts
@@ -1,10 +1,28 @@
+export interface Reply { //대댓글
+    id: string;
+    userimgUrl: string;
+    userName: string;
+    content: string;
+    createdAt: string;
+}
+
+export interface Comment { //댓글
+    id: string;
+    userimgUrl: string;
+    userName: string;
+    content: string;
+    commentlikes: number;
+    createdAt: string;
+    replies?: Reply[];
+}
+
 export interface Commu {
     /* 데이터 타입지정 */
     id: string,
     title: string,
     description: string,
     category: string,
-    imageUrl?:string,
+    imageUrl?: string,
     likes: number,
     createdAt: string,
     views: number,
@@ -12,12 +30,5 @@ export interface Commu {
     userimgUrl: string,
     userName: string,
     commentsNum: number,
-    comments: {
-        id: string, 
-        userimgUrl: string,
-        userName: string,
-        content: string,
-        commentlikes: number,
-        createdAt: string,
-    }[];
+    comments: Comment[];
 }
